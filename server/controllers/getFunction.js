16617783018ec3ca891d68f95c1196793f999c3b
dahiya001rohit <rohit830770@gmail.com>
2026-01-27 12:10:43 +0530

    made functions to extract data

--- server/controllers/getFunction.js
+++ server/controllers/getFunction.js
@@ -4,6 +4,7 @@ async function getTrack(req, res){
     const userId = req.user.userId
     try {
         const track = await DailyTrack.find( { userId: userId })
+        console.log(track)
         return res.json({ track })
     } catch (error) {
         return res.json({ error })
